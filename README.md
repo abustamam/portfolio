7eacb90bd60f57869006afe4062129807c0413d8
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2014-05-14 13:48:58 -0700

    Improve README

---
+++ README.md
@@ -0,0 +1,9 @@
+Rasheed Bustamam, web developer
+===
+
+XXX
+---
+
+* Ruby 2.1.1
+
+* System dependencies
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Portfolio::Application.config.secret_key_base = '2bd0c1cf451ad51e4814eccb4145df2a0c88f6df170e3f22e20c3ac8b2a2f02c0387e006881b09d577b1c81c4204a65d989a30ec535bd9d059b2d71b60e40648'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
